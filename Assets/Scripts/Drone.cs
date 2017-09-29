c0cfc6b60be19efbf8bc876d880f664d5a2e6c77
박민근 <agebreak@naver.com>
2017-09-29 21:39:20 +0900

    Initial Commit

---
+++ Assets/Scripts/Drone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Drone : MonoBehaviour {
+    public Transform tower; // 목적지 타워
+
+	// Use this for initialization
+	void Start () {
+        // 타워 오브젝트를 찾아서, 세팅한다. 
+        tower = GameObject.Find("Tower").GetComponent<Transform>();
+
+        // 네비게이션 목적지를 타워의 위치로 설정한다. 
+        GetComponent<NavMeshAgent>().destination = tower.position;
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    // 스폰 위치들을 받는다 (배열)
+    public Transform[] spawnPoints;
+    public GameObject drone;        // 생성할 드론 프리팹
+    public float spawnTime;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnDrone", 0.0f, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnDrone()
+    {
+        // 스폰 포인트 중에서 램던한 위치에서 드론을 생성한다. 
+        int index = Random.Range(0, spawnPoints.Length);
+        Vector3 pos = spawnPoints[index].position;
+
+        GameObject newDrone = Instantiate(drone);
+        newDrone.transform.position = pos;
+    }
+}
