a8d22542ee0e1346da69fa69c297326945cf43f1
박민근 <agebreak@naver.com>
2017-10-27 19:56:48 +0900

    10/27 업데이트

---
+++ Assets/ElfRun/ElfPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElfPlayer : MonoBehaviour {
+    public float speed;
+
+    Vector3 moveVec;    // 이동하는 벡터
+    CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVec = transform.forward * speed;
+        controller.SimpleMove(moveVec);
+	}
+}
